c6163d46f4e6e032dd9abf9b481bda341c92d23b
Damien Bachet <dbachet@gmail.com>
2013-04-23 21:38:27 +0200

    Create rails 4 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Changeons::Application.config.secret_key_base = '519c957b43632422ed6c0d3137b48bca37ade4589580308440b9f620b35657599006de7c204cb49c736758c12bbd0fbbadefafbddfbb9d20940817af55ab00aa'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Changeons::Application.config.session_store :encrypted_cookie_store, key: '_changeons_session'
