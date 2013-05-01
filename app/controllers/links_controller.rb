ebc47c45d7e9c672377de7d97ab85f692ec8a4ff
Damien Bachet <dbachet@gmail.com>
2013-05-01 22:43:03 +0200

    User must be connected to access some pages

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,4 +1,5 @@
 class LinksController < ApplicationController
+  before_filter :authenticate_user!, except: [:show, :index]
   load_and_authorize_resource
   before_action :set_link, only: [:show, :edit, :update, :destroy]
 
