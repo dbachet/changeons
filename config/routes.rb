7bc52997eb348ad6a291bdf3f8db0f7358a5dfd0
Damien Bachet <dbachet@gmail.com>
2013-05-05 21:47:00 +0200

    Add index user page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Changeons::Application.routes.draw do
   devise_for :users
+  resources :users
   resources :links
 
   # The priority is based upon order of creation: first created -> highest priority.
