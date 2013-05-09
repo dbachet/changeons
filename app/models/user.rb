21197e33bbfdb41fe6d3cc72b2f56011c7354f0a
Damien Bachet <dbachet@gmail.com>
2013-05-09 19:34:28 +0200

    Add a default member role if no role defined

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  after_create :add_default_role
 
   ROLES = %w(member admin)
 
@@ -15,4 +16,12 @@ class User < ActiveRecord::Base
   def member?
     @member ||= has_role?(:member)
   end
+
+  private
+
+    def add_default_role
+      if role_ids.empty?
+        add_role(ROLES.first)
+      end
+    end
 end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'User' do
+  let(:user) { build :user }
+
+  subject { user }
+
+  describe 'testing validations' do
+    it { should be_valid }
+    it { subject.save.should be_true }
+  end
+
+  describe 'check roles after creation' do
+    before { user.save }
+
+    it { subject.roles_name.should eql ['member'] }
+  end
+end
