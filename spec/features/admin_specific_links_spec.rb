827fb0d7e5e74c4f0dfe6bdfaa91a9f57f91dcbe
Damien Bachet <dbachet@gmail.com>
2013-05-11 23:46:20 +0200

    Add a link to user index for admin

---
+++ spec/features/admin_specific_links_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+feature 'Admin specific links' do
+  background { sign_in :admin }
+
+  scenario "Have a link to user index" do
+    visit root_path
+
+    click_link 'Users'
+    current_path.should == users_path
+  end
+end
