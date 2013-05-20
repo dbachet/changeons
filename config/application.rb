6c9e03d0733f8d0bd724588192c150e8bb15d80c
Damien Bachet <dbachet@gmail.com>
2013-05-20 20:20:04 +0200

    Refactor params

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module Changeons
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**/*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :fr
 
     # rails-I18n
