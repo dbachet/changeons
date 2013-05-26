bf7d62b20664db8dcb7d71bc065661251eb9a986
Damien Bachet <dbachet@gmail.com>
2013-05-26 23:30:07 +0200

    Add welcome email when user registers itsef (via postageapp)

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -2,7 +2,23 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   def create
     super
-    UserMailer.welcome(@user).deliver unless @user.invalid?
+
+    # TODO: Move this code in a cleaner place !!
+    request = PostageApp::Request.new(:send_message, {
+      'headers' => {
+        'subject'  => t('.welcome.subject')
+      },
+      'recipients'  => {
+        @user.email => {
+          hello: t('.welcome.hello', name: @user.name),
+          you_can_login: t('.welcome.you_can_login'),
+          account_created_successfully: t('.welcome.account_created_successfully'),
+          greetings: t('.welcome.greetings')
+        }
+      },
+      'template' => 'welcome'
+    })
+    response = request.send
   end
 
 end
