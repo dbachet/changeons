b964d4b2b4aeee2f50df37ac87d9238f08640490
Damien Bachet <dbachet@gmail.com>
2013-05-27 23:38:15 +0200

    Extract secret token to env var

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Changeons::Application.config.secret_key_base = '519c957b43632422ed6c0d3137b48bca37ade4589580308440b9f620b35657599006de7c204cb49c736758c12bbd0fbbadefafbddfbb9d20940817af55ab00aa'
+Changeons::Application.config.secret_key_base = ENV['SECRET_TOKEN']
