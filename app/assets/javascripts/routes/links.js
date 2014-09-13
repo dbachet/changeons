e296e68d977c3fa1e0c320ed226a9f09cfedef9c
Damien Bachet <dbachet@gmail.com>
2014-09-13 19:52:36 +0200

    Arrange queryParams stuff

--- app/assets/javascripts/routes/links.js
+++ app/assets/javascripts/routes/links.js
@@ -5,23 +5,27 @@ Ch.LinksRoute = Ember.Route.extend({
     }
   },
   model: function (params) {
-    return this.store.findQuery('link', params);
+    if (params) {
+      return this.store.findQuery('link', params);
+    } else {
+      return this.store.find('link');
+    }
   },
   renderTemplate: function () {
     this._super.apply(this, arguments);
 
     var categoriesController = this.controllerFor('categories');
 
-    // Fecth and render categories only if it hasn't been already
+    // Fecth categories only if it hasn't been already
     if (Ember.isEmpty(categoriesController.get('model'))) {
       categoriesController.set('model', this.store.find('category'));
-
-
-      return this.render('sidebar', {
-        outlet: 'sidebar',
-        into: 'application',
-        controller: categoriesController
-      });
     }
+
+    return this.render('sidebar', {
+      outlet: 'sidebar',
+      into: 'application',
+      controller: categoriesController
+    });
+
   }
 });
