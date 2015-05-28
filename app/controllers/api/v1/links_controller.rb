1b44aece6100c31bdfdace216b69c58654cdd0b1
Damien Bachet <dbachet@gmail.com>
2015-05-28 21:37:43 +0200

    reorder links + change amount of links per page

--- app/controllers/api/v1/links_controller.rb
+++ app/controllers/api/v1/links_controller.rb
@@ -5,14 +5,14 @@ module Api
       def index
 
         page = (params[:page] || 1).to_i
-        links = Link.all
+        links = Link.order(id: :desc)
         category = Category.where(name: params[:category]).first
 
         if category.present?
           links = links.where(category: category)
         end
 
-        links = links.page(page).per(25)
+        links = links.page(page).per(10)
 
         render json: links, each_serializer: LinkSerializer, meta: {total_pages: links.total_pages}
       end
