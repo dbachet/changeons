c1f63ca9b00b3c6c42bf3e24b19878e687a31a20
Damien Bachet <dbachet@gmail.com>
2015-06-03 10:38:19 +0200

    Add TweetsService#persist_tweets_to_db

---
+++ app/services/tweets_service.rb
@@ -0,0 +1,20 @@
+class TweetsService
+
+  attr_reader :tweets
+
+  def initialize(opts = {})
+    @tweets = opts.fetch(:tweets, [])
+  end
+
+  def persist_tweets_to_db
+    Tweet.create(tweets_attributes_array)
+  end
+
+  private
+
+  def tweets_attributes_array
+    tweets.map do |tweet|
+      { remote_id: tweet.id }
+    end
+  end
+end
